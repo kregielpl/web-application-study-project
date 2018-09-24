e0653e72276d325db2da0cdf3266e8f1142ecde7
Daniel <matii262@interia.pl>
2018-09-24 07:39:50 +0200

    Wrzutka bazowa

---
+++ server/src/test/java/pl/student/webaplicationstudyproject/WebAplicationStudyProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.student.webaplicationstudyproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebAplicationStudyProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
